ab7ed8357ade16bb1b6a038e9c68d2bfe60f6f83
Sayyam Butt <126108224+sayyamButt317@users.noreply.github.com>
2024-10-14 01:49:55 -0700

    add the frontend landing design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
     },
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      '2xl': ['1.5rem', {
+        lineHeight: '2rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      '3xl': ['1.875rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+    }
   },
   plugins: [],
 };
